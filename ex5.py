551f5cca23c2b5ced8c8895adffe2b78484c7879
Raitis <43616698+Raitis128@users.noreply.github.com>
2022-01-27 20:55:58 +0300

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,9 @@
+def bool_to_word(boolean):
+    if boolean == 1:  # True == 1
+        print('Yes')
+    elif boolean == 0:  # False == 0
+        print('No')
+
+
+boolean = False
+bool_to_word(boolean)
